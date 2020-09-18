dd43e662f98cf44ac4a2397f1f9b27ba96edb917
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-09-18 14:41:14 +0200

    Updates

---
+++ precache-manifest.7caf9e652c15e44612c2b0aaec7d8f33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126df0de89b3044e4991270c8f61b8b8",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "2463e95a7bf84a165627",
+    "url": "/pokemon-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3939a5eac779bec02bc",
+    "url": "/pokemon-app/static/js/2.2dd0531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-app/static/js/2.2dd0531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2463e95a7bf84a165627",
+    "url": "/pokemon-app/static/js/main.f9eef772.chunk.js"
+  },
+  {
+    "revision": "53b9c226502149a401b8",
+    "url": "/pokemon-app/static/js/runtime-main.9ee58c44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemon-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
